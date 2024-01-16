15de3157b14e54b78de0bd407048f48db8efa886
unknown <business.urfavteddybear@gmail.com>
2024-01-16 21:02:26 +0800

    test: forbidden error

--- app/Http/Controllers/bookCategoryController.php
+++ app/Http/Controllers/bookCategoryController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\storeBookCategoryRequest;
+use App\Http\Requests\StoreBookCategoryRequest;
 use App\Models\bookCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
@@ -17,49 +17,11 @@ class bookCategoryController extends Controller
         return Response($show_category, 200);
     }
 
-    public function store(storeBookCategoryRequest $request)
+    public function store(StoreBookCategoryRequest $request)
     {
-        //create post
         bookCategory::create([
             'category'=> $request->category,
         ]);
     }
-
-    
-
-    // public function store(storeBookCategoryRequest $request)
-    // {
-
-    //     $bookCategory = bookCategory::create([
-    //         'category' => $request->category
-    //     ]);
-
-    //     return new $bookCategory;
-    // }
-
-    // public function store(storeBookCategoryRequest $request) 
-    // {
-    // 	$bookCategory = $request->category([
-    //         'category' => 'required'
-    //     ]);
-
-    //     bookCategory::create($bookCategory);
-
-    //     return response()->json(['success'=>'Laravel ajax example is being processed.']);
-    // }
-
-    // public function store(storeBookCategoryRequest $request)
-    // {
-    //     $bookCategory = bookCategory::create([
-    //         'category' => $request->category
-    //     ]);
-    //     // $post = Post::create($request->all());
-
-    //     return response()->json([
-    //         'status' => true,
-    //         'message' => "Post Created successfully!",
-    //         'post' => $bookCategory
-    //     ], 200);
-    // }
 }
 
--- app/Http/Requests/storeBookCategoryRequest.php
+++ app/Http/Requests/storeBookCategoryRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class storeBookCategoryRequest extends FormRequest
+class StoreBookCategoryRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
